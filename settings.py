84cdc52d15781e6df260d6c6b6a4f3a2cc6cc82f
Andrian Buzdanov <82593138+redl1k@users.noreply.github.com>
2022-08-15 22:19:14 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,2 +1,2 @@
-TG_TOKEN = ("5048860066:AAGrGUWyVGTiJS1lPbtue3ari8bVzKfaPeo")
-TG_API_URL ="https://api.telegram.org/bot5048860066:AAGrGUWyVGTiJS1lPbtue3ari8bVzKfaPeo"
+TG_TOKEN = ("Ur  Token")
+TG_API_URL ="https://api.telegram.org/ur bot token
